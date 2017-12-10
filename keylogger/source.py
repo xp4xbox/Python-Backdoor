c7203316276c02ab2a7420eb5f078bae0a2322eb
xp4xbox <nicman1080@gmail.com>
2017-12-10 12:39:46 -0500

    Update source.py

--- keylogger/source.py
+++ keylogger/source.py
@@ -1,3 +1,7 @@
+'''
+Keylogger source, Python 2.7
+Referenced from my other project: https://github.com/xp4xbox/Puffader
+'''
 import time, os, threading
 import win32console, win32gui, win32event, win32api, winerror
 from sys import exit

bdedd928da01434d47ab7e88b8a59a0bea85eb15
xp4xbox <nicman1080@gmail.com>
2018-04-16 13:16:20 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -7,16 +7,23 @@ objSocket.connect(("google.com", 0))
 strCurrentIP = objSocket.getsockname()[0]
 objSocket.close()
 
+
 print("1. Use: " + strCurrentIP)
 print("2. Use a different IP address for server")
 print("3. Use a DNS Hostname")
 
 strChoice = input("\n" + "Type selection: ")
 
-if strChoice == "2":
+if strChoice == "1":
+    pass
+elif strChoice == "2":
     strCurrentIP = input("\n" + "Enter IP: ")
 elif strChoice == "3":
     strDNSHostname = input("\n" + "Enter DNS Hostname: ")
+else:
+    print("Invalid Choice!")
+    sys.exit(0)
+
 
 objClientFile = open("client.py", "r")
 arrFileContents = objClientFile.readlines()
@@ -24,13 +31,31 @@ objClientFile.close()
 
 if strChoice == "2" or strChoice == "1":
     arrFileContents[6] = "strHost = \"" + strCurrentIP + "\"" + "\n"
+    arrFileContents[7] = "#strHost = socket.gethostbyname(\"\")" + "\n"
 else:
+    arrFileContents[6] = "#strHost = \"\"" + "\n"
     arrFileContents[7] = "strHost = socket.gethostbyname(\"" + strDNSHostname + "\")" + "\n"
 
 objClientFile = open("client.py", "w")
 objClientFile.writelines(arrFileContents)
 objClientFile.close()
 
-os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
-                        "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
-                        "--onefile --windowed")
+
+strIconChoice = input("\n" + "Path for icon (x to skip): ")
+
+strIconChoice = strIconChoice.replace("\"", "")
+
+if strIconChoice == "x":
+    os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
+                      "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
+                      "--onefile --windowed")
+elif not os.path.isfile(strIconChoice):
+    print("Invalid path!")
+    sys.exit(0)
+elif not strIconChoice.endswith(".ico"):
+    print("Must be a .ico file!")
+    sys.exit(0)
+else:
+    os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
+                      "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
+                      "--onefile --windowed --icon=\"" + strIconChoice + "\"")

